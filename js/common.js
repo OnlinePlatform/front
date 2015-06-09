08a29762364ce09ede1f767271a34174d41cb2e2
Amy <1310009181@qq.com>
2015-06-09 12:51:29 +0800

    add some new pages
    add some new pages

--- js/common.js
+++ js/common.js
@@ -7,4 +7,9 @@ $(function(){
         var id = $(this).attr("href");
         $(id).removeClass("hidden").siblings().addClass("hidden"); //显示当前选中的链接及关联的content
     });
+    $('#user').hover(function(){
+    	$('.user-center').removeClass('hidden');
+    },function(){
+    	$('.user-center').addClass('hidden');
+    });
 });
