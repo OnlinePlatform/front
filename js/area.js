81483635982aff955f1ef5b27173c550a54ac0dc
wkd <1291769238@qq.com>
2015-07-08 16:53:02 +0800

    conflix fix

--- js/area.js
+++ js/area.js
@@ -9,7 +9,8 @@ function showLocation(province , city , town) {
 	$('#loc_province').append(title[0]);
 	$('#loc_city').append(title[1]);
 	$('#loc_town').append(title[2]);
-	
+	$('#census_province').append(title[0]);
+	$('#census_city').append(title[1]);
 	
 	$('#loc_province').change(function() {
 		$('#loc_city').empty();
@@ -19,7 +20,11 @@ function showLocation(province , city , town) {
 		$('#loc_town').append(title[2]);
 		//$('input[@name=location_id]').val($(this).val());
 	})
-	
+	$('#census_province').change(function() {
+		$('#census_city').empty();
+		$('#census_city').append(title[1]);
+		loc.fillOption('census_city' , '0,'+$('#census_province').val());
+	})
 	$('#loc_city').change(function() {
 		$('#loc_town').empty();
 		$('#loc_town').append(title[2]);
@@ -32,10 +37,11 @@ function showLocation(province , city , town) {
 	})
 	if (province) {
 		loc.fillOption('loc_province' , '0' , province);
+		loc.fillOption('census_province' , '0' , province);
 		
 		if (city) {
 			loc.fillOption('loc_city' , '0,'+province , city);
-			
+			loc.fillOption('census_city' , '0,'+province , city);
 			if (town) {
 				loc.fillOption('loc_town' , '0,'+province+','+city , town);
 			}
@@ -43,5 +49,7 @@ function showLocation(province , city , town) {
 		
 	} else {
 		loc.fillOption('loc_province' , '0');
+		loc.fillOption('census_province' , '0');
+
 	}	
 }
